50491f1c1622846bb45748a9577fb1ad2ffdd400
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-17 20:41:14 +0100

    Initialize glfw

--- engine/windowing_system/window_manager.hpp
+++ engine/windowing_system/window_manager.hpp
@@ -1,21 +1,34 @@
+//@{"dependencies_extra":[{"ref":"glfw3", "rel":"implementation", "origin":"pkg-config"}]}
+
 #ifndef IDIS_WINDOWINGSYSTEM_WINDOWMANAGER_HPP
 #define IDIS_WINDOWINGSYSTEM_WINDOWMANAGER_HPP
 
 #include "error_handler/exception.hpp"
+
+#include <GLFW/glfw3.h>
 
 namespace idis
 {
 	class window_manager
 	{
 	public:
-		window_manager() { throw exception{"initialize window manager", "not implemented"}; }
+		window_manager()
+		{
+			if(glfwInit() != GLFW_TRUE) [[unlikely]]
+			{
+				char const* cause = nullptr;
+				glfwGetError(&cause);
+				throw exception{"initialize the window manager",
+				                cause != nullptr ? cause : "Unknown error"};
+			}
+		}
 
 		window_manager(window_manager const&) = delete;
 		window_manager(window_manager&&)      = delete;
 		window_manager& operator=(window_manager const&) = delete;
 		window_manager& operator=(window_manager&&) = delete;
 
-		~window_manager() {}
+		~window_manager() { glfwTerminate(); }
 	};
 }
 
