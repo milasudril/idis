a05d544984891da8b149e11c85ab5a81ca282d8c
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-19 17:38:27 +0100

    Add testcase for create window failed

--- engine/window_manager/window.test.cpp
+++ engine/window_manager/window.test.cpp
@@ -32,3 +32,38 @@ TESTCASE(idis_wm_window_create)
 
 	idis::wm::glfw_stub::overrides.destroy_window = [](GLFWwindow* window) { delete window; };
 }
+
+TESTCASE(idis_wm_window_create_fail)
+{
+	idis::wm::glfw_stub::overrides.init      = []() { return GLFW_TRUE; };
+	idis::wm::glfw_stub::overrides.terminate = []() {};
+	idis::wm::glfw_stub::overrides.window_hint = [](int key, int value) {
+		EXPECT_EQ(key, GLFW_CLIENT_API);
+		EXPECT_EQ(value, GLFW_NO_API);
+	};
+	idis::wm::glfw_stub::overrides.create_window =
+	    [](int width, int height, char const* title, GLFWmonitor* monitor, GLFWwindow* share)
+	{
+		EXPECT_EQ(width, 800);
+		EXPECT_EQ(height, 500);
+		EXPECT_EQ(strcmp(title, "Foobar"), 0);
+		EXPECT_EQ(monitor, nullptr);
+		EXPECT_EQ(share, nullptr);
+
+		return static_cast<GLFWwindow*>(nullptr);
+	};
+	idis::wm::glfw_stub::overrides.get_error = [](char const** msg) {
+		*msg = "Some error";
+		return 0;
+	};
+
+	try
+	{
+		idis::wm::window win{800, 500, "Foobar"};
+		TestFwk::currentTestcase->testcaseFailed();
+	}
+	catch(idis::exception const& e)
+	{
+		EXPECT_EQ(strcmp(e.what(), "Failed to Failed to create a new window: Some error"), 0);
+	}
+}
