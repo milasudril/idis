1ce0d4633e4295f9b6a9e015f8bde40172bcb1eb
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-20 17:58:11 +0100

    Add timepoint testcases

--- engine/event_sequencer/timepoint.hpp
+++ engine/event_sequencer/timepoint.hpp
@@ -9,42 +9,42 @@ namespace idis::seq
 	class timepoint
 	{
 	public:
-		constexpr explicit timepoint(uint64_t val): m_value{val} {}
+		constexpr explicit timepoint(int64_t val): m_value{val} {}
 
 		constexpr auto operator<=>(timepoint const& other) const = default;
 
 		constexpr auto value() const { return m_value; }
 
-		constexpr void tick() { ++m_value; }
+		constexpr void step() { ++m_value; }
 
 	private:
-		uint64_t m_value;
+		int64_t m_value;
 	};
 
-	constexpr uint64_t timepoints_per_second = 60;
+	constexpr int64_t timepoints_per_second = 60;
 
 	constexpr int64_t operator-(timepoint a, timepoint b) { return a.value() - b.value(); }
 
 	constexpr auto operator+(timepoint a, int64_t t)
 	{
-		return timepoint{static_cast<uint64_t>(a.value() + t)};
+		return timepoint{a.value() + t};
 	}
 
 	constexpr auto operator-(timepoint a, int64_t t)
 	{
-		return timepoint{static_cast<uint64_t>(a.value() - t)};
+		return timepoint{a.value() - t};
 	}
 
-	inline std::string to_string(timepoint t) { return std::to_string(t); }
+	inline std::string to_string(timepoint t) { return std::to_string(t.value()); }
 
-	double seconds_between(timepoint a, timepoint b)
+	inline double seconds_between(timepoint a, timepoint b)
 	{
 		return static_cast<double>(a - b) / static_cast<double>(timepoints_per_second);
 	}
 
-	double seconds_since_start(timepoint a)
+	inline double seconds_since_start(timepoint a)
 	{
-		return seconds_between(timepoint{}, a);
+		return seconds_between(timepoint{0}, a);
 	}
 }
 
---
+++ engine/event_sequencer/timepoint.test.cpp
@@ -0,0 +1,35 @@
+//@{"target":{"name":"timepoint.test"}}
+
+#include "./timepoint.hpp"
+
+#include "ext_proj/testfwk/testfwk.hpp"
+
+TESTCASE(idis_seq_timepoint_step)
+{
+	idis::seq::timepoint start{1};
+	EXPECT_EQ(start.value(), 1);
+	start.step();
+	EXPECT_EQ(start, (idis::seq::timepoint{2}));
+}
+
+TESTCASE(idis_seq_timepoint_add_offset)
+{
+	idis::seq::timepoint start{1};
+	EXPECT_EQ(start.value(), 1);
+	EXPECT_EQ(start + 50, (idis::seq::timepoint{51}));
+}
+
+TESTCASE(idis_seq_timepoint_subtract_offset)
+{
+	idis::seq::timepoint start{1};
+	EXPECT_EQ(start.value(), 1);
+	EXPECT_EQ(start - 50, (idis::seq::timepoint{-49}));
+}
+
+TESTCASE(idis_seq_timepoint_difference)
+{
+	idis::seq::timepoint a{5};
+	idis::seq::timepoint b{2};
+	auto delta = a - b;
+	EXPECT_EQ(delta, 3);
+}
