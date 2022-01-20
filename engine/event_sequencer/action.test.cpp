309cd2ed557d4b5df5160966f1d86a4ec6a35ada
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-20 20:37:27 +0100

    Write testcase

--- engine/event_sequencer/action.test.cpp
+++ engine/event_sequencer/action.test.cpp
@@ -3,3 +3,11 @@
 #include "./action.hpp"
 
 #include "ext_proj/testfwk/testfwk.hpp"
+
+TESTCASE(idis_seq_action_create_and_call)
+{
+	bool called = false;
+	idis::seq::action action{[&called](){called = true;}};
+	action();
+	EXPECT_EQ(called, true);
+}
