dc63aa0a833723a1a7d2ae17ad248a16b0a18a46
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-20 18:09:45 +0100

    Add event index class

---
+++ engine/event_sequencer/event_index.hpp
@@ -0,0 +1,26 @@
+#ifndef IDIS_EVENTSEQUENCER_EVENTINDEX_HPP
+#define IDIS_EVENTSEQUENCER_EVENTINDEX_HPP
+
+#include <string>
+
+namespace idis::seq
+{
+	class event_index
+	{
+	public:
+		constexpr explicit event_index(int val): m_value{val} {}
+
+		constexpr auto operator<=>(event_index const& other) const = default;
+
+		constexpr auto value() const { return m_value; }
+
+		constexpr void next() { ++m_value; }
+
+	private:
+		int m_value;
+	};
+
+	inline std::string to_string(event_index t) { return std::to_string(t.value()); }
+}
+
+#endif
