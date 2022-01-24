e2c217ce5431089c563138648d463a7b2ea207b3
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-24 19:46:15 +0100

    One more testcase

--- engine/event_sequencer/event_queue.test.cpp
+++ engine/event_sequencer/event_queue.test.cpp
@@ -22,7 +22,7 @@ namespace
 	};
 }
 
-TESTCASE(idis_seq_event_queue_push_and_drain_seq)
+TESTCASE(idis_seq_event_queue_push_and_drain_all_in_seq)
 {
 	idis::seq::event_queue events;
 	EXPECT_EQ(events.empty(), true);
@@ -60,6 +60,50 @@ TESTCASE(idis_seq_event_queue_push_and_drain_seq)
 
 	EXPECT_EQ(events.empty(), true);
 	EXPECT_EQ(std::size(out), std::size(timepoints));
+	for(size_t k = 0; k != std::size(out); ++k)
+	{
+		EXPECT_EQ(k, out[k]);
+	}
+}
+
+TESTCASE(idis_seq_event_queue_push_and_drain_until)
+{
+	idis::seq::event_queue events;
+	EXPECT_EQ(events.empty(), true);
+
+	std::array<idis::seq::timepoint, 16> timepoints{idis::seq::timepoint{idis::seq::tick{2}},
+	                                                idis::seq::timepoint{idis::seq::tick{3}},
+	                                                idis::seq::timepoint{idis::seq::tick{0}},
+	                                                idis::seq::timepoint{idis::seq::tick{1}},
+	                                                idis::seq::timepoint{idis::seq::tick{7}},
+	                                                idis::seq::timepoint{idis::seq::tick{4}},
+	                                                idis::seq::timepoint{idis::seq::tick{5}},
+	                                                idis::seq::timepoint{idis::seq::tick{6}},
+	                                                idis::seq::timepoint{idis::seq::tick{7}},
+	                                                idis::seq::timepoint{idis::seq::tick{3}},
+	                                                idis::seq::timepoint{idis::seq::tick{2}},
+	                                                idis::seq::timepoint{idis::seq::tick{1}},
+	                                                idis::seq::timepoint{idis::seq::tick{4}},
+	                                                idis::seq::timepoint{idis::seq::tick{6}},
+	                                                idis::seq::timepoint{idis::seq::tick{5}},
+	                                                idis::seq::timepoint{idis::seq::tick{0}}};
+
+	std::ranges::sort(timepoints);
+
+	std::vector<size_t> out;
+	std::ranges::for_each(timepoints,
+	                      [&events, k = static_cast<size_t>(0), &out](auto t) mutable
+	                      {
+		                      events.push(t, action{k, out});
+		                      ++k;
+	                      });
+
+	EXPECT_EQ(events.empty(), false);
+
+	drain(events, idis::seq::timepoint{idis::seq::tick{3}});
+
+	EXPECT_EQ(events.empty(), false);
+	EXPECT_EQ(std::size(out), std::size(timepoints)/2);
 	for(size_t k = 0; k != std::size(out); ++k)
 	{
 		EXPECT_EQ(k, out[k]);
