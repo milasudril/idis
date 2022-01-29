0fd609eee3d513f2a6a07e2e923828a01ae048ad
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-29 14:42:54 +0100

    Initial work on error message render

--- engine/app/idis.cpp
+++ engine/app/idis.cpp
@@ -4,6 +4,11 @@
 #include "engine/window_manager/cairo_surface.hpp"
 #include "engine/event_sequencer/event_loop.hpp"
 
+#include "fruit/lib/text_line.hpp"
+#include "fruit/lib/font_mapper.hpp"
+#include "fruit/lib/font_face.hpp"
+#include "fruit/lib/io_utils.hpp"
+
 #include <cstdio>
 
 struct window_action_tag
@@ -14,6 +19,12 @@ struct message_display
 {
 	idis::seq::event_loop event_loop;
 	idis::wm::cairo_surface draw_surface;
+
+	message_display(std::reference_wrapper<fruit::FontFace const> font):
+		message{font}
+	{}
+
+	fruit::TextLine message;
 };
 
 void window_closed(message_display& obj, window_action_tag)
@@ -29,7 +40,12 @@ void window_size_changed(message_display& obj, window_action_tag, idis::wm::dime
 void present(std::exception const& e)
 try
 {
-	message_display md;
+	fruit::FontMapper font_mapper;
+	auto font_file = font_mapper.get_path("mono-space");
+	fruit::FontfaceLoader font_loader;
+	fruit::FontFace font_face{font_loader, fruit::io_utils::load(font_file)};
+	message_display md{font_face};
+	md.message.text(reinterpret_cast<char8_t const*>(e.what()));
 	idis::wm::window mainwin{md, 800, 500, "Idis"};
 	mainwin.set_close_callback<window_action_tag>().set_size_callback<window_action_tag>();
 	md.event_loop.set_pre_drain_callback(glfwWaitEvents);
