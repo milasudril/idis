10b6d0048fc16a876c79e66fc6b680d5468391e9
Torbjörn Rathsman <milasudril@gmail.com>
2022-02-08 18:11:33 +0100

    Keep vertex type after compilation

--- engine/shaders/repo.cpp
+++ engine/shaders/repo.cpp
@@ -1,17 +1,17 @@
 //@	{
 //@		"target":{"name":"repo.o"}
 //@		,"dependencies":[
-//@			{"ref":"./my_vertex_shader.spv", "origin":"generated"},
-//@			{"ref":"./my_fragment_shader.spv", "origin":"generated"}
+//@			{"ref":"./my_vertex_shader.vert.spv", "origin":"generated"},
+//@			{"ref":"./my_fragment_shader.frag.spv", "origin":"generated"}
 //@		]
 //@	}
 
 #include "./repo.hpp"
 #include "engine/helpers/incbin.hpp"
 
-IDIS_INCBIN_U32(vertex_shader, MAIKE_BUILDINFO_TARGETDIR "/engine/shaders/my_vertex_shader.spv");
+IDIS_INCBIN_U32(vertex_shader, MAIKE_BUILDINFO_TARGETDIR "/engine/shaders/my_vertex_shader.vert.spv");
 IDIS_INCBIN_U32(fragment_shader,
-                MAIKE_BUILDINFO_TARGETDIR "/engine/shaders/my_fragment_shader.spv");
+                MAIKE_BUILDINFO_TARGETDIR "/engine/shaders/my_fragment_shader.frag.spv");
 
 std::span<uint32_t const> idis::shaders::repo::get_vertex_shader()
 {
